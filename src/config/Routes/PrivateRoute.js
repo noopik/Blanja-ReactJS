5efcca60b99203333a2b779c9f594e3290b62681
dwinovic <nd.cahya19@gmail.com>
2021-08-03 00:53:14 +0700

    add private and public route

--- src/config/Routes/PrivateRoute.js
+++ src/config/Routes/PrivateRoute.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+import { Axios } from '..';
+import { Toast } from '../../components/atoms';
+import { typeRedux } from '../../utils';
 
-const PrivateRoute = () => {
-  return <div></div>;
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const [isLogin, setIsLogin] = useState({ check: false, passed: false });
+  const token = localStorage.getItem('token');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    Axios.get(`/users/verify-token`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((result) => {
+        const dataResult = result.data.data;
+        setIsLogin({ check: true, passed: true });
+        dispatch({ type: typeRedux.setUserLogin, value: dataResult });
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLogin({ check: true, passed: false });
+        return Toast(
+          `Uppss, you don't have access! Please login before`,
+          'error'
+        );
+      });
+  }, []);
+
+  console.log(isLogin);
+  return (
+    <>
+      {isLogin.check && (
+        <Route
+          {...rest}
+          render={(props) => {
+            return isLogin.passed ? (
+              <Component {...props} />
+            ) : (
+              <Redirect to="/customer-login" />
+            );
+          }}
+        />
+      )}
+    </>
+  );
 };
 
 export default PrivateRoute;
--- src/config/Routes/index.js
+++ src/config/Routes/index.js
@@ -22,6 +22,7 @@ import {
   SellerLogin,
   SellerRegister,
 } from '../../pages/AuthPages';
+import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
 const Routes = () => {
@@ -37,37 +38,40 @@ const Routes = () => {
         <Route path="/forgot-password/:token" component={ForgotPassword} />
         {/* MAIN HOME */}
         <PublicRoute exact path="/" component={Homepage} />
-        <Route path="/categories" component={CategoryPage} />
-        <Route path="/products" component={ResultProducts} />
-        <Route path="/my-bag" component={MyBag} />
-        <Route path="/checkout" component={CheckoutPage} />
+        <PrivateRoute path="/categories" component={CategoryPage} />
+        <PrivateRoute path="/products" component={ResultProducts} />
+        <PrivateRoute path="/my-bag" component={MyBag} />
+        <PrivateRoute path="/checkout" component={CheckoutPage} />
         <Route
           path="/users-verified/:token"
           component={VerifiedRegisterSuccess}
         />
         {/* USER CUSTOMER */}
-        <Route path="/user/setting" component={UserProfileSetting} />
-        <Route path="/user/shipping-address" component={UserProfileAddress} />
-        <Route path="/user/orders" component={UserProfileOrder} />
+        <PrivateRoute path="/user/setting" component={UserProfileSetting} />
+        <PrivateRoute
+          path="/user/shipping-address"
+          component={UserProfileAddress}
+        />
+        <PrivateRoute path="/user/orders" component={UserProfileOrder} />
         {/* USER SELLER */}
-        <Route exact path="/admin/seller" component={SellerStore} />
-        <Route
+        <PrivateRoute exact path="/admin/seller" component={SellerStore} />
+        <PrivateRoute
           exact
           path="/admin/seller/products"
           component={SellerProductPage}
         />
-        <Route
+        <PrivateRoute
           exact
           path="/admin/seller/selling"
           component={SellerSellingProducts}
         />
-        <Route
+        <PrivateRoute
           path="/admin/seller/selling/:slug"
           component={SellerSellingProducts}
         />
-        <Route path="/admin/seller/orders" />
-        <Route path="/admin/seller#cancel" />
-        <Route path="/product/:name-:id" component={ProductDetailPage} />
+        <PrivateRoute path="/admin/seller/orders" />
+        <PrivateRoute path="/admin/seller#cancel" />
+        <PrivateRoute path="/product/:name-:id" component={ProductDetailPage} />
       </Switch>
     </Router>
   );
